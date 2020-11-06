ddc971899486c645cd1b47082e881e4f07e575c6
Nico30d <Nico30d@gmail.com>
2020-11-06 10:22:24 +0100

    feat(*): init

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7f3a43d876e22e69e1495236a0af9389'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e1aed9ce-b68d-4aca-b882-1992d93ba851'
+};
