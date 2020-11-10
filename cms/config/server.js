311356f4a9cbd966bd16ddfbc097ed5cb2da9831
PBWozniak <piotr.bartosz.wozniak@gmail.com>
2020-11-10 12:56:21 +0100

    Zmiana hosta i portu

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', 'localhost'),
+  port: env.int('PORT', 5000),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '7f3a43d876e22e69e1495236a0af9389'),
