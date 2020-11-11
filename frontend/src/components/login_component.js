e27f37011df150952e72e7b6d0aae8298d24feb4
PBWozniak <piotr.bartosz.wozniak@gmail.com>
2020-11-11 14:27:24 +0100

    Dodanie gatsby-plugin-styled-components, stworzenie szablonu logowania w html'u

--- frontend/src/components/login_component.js
+++ frontend/src/components/login_component.js
@@ -1,7 +1,14 @@
 import React from "react"
+import styled from 'styled-components'
 
 const Login = () => {
-    return <div>welcome in login</div>    
+    return <div>
+                <h1>Login</h1>
+                <input type="email" name="mail input" placeholder="Your student mail"/>
+                <br/>
+                <input type="submit" name="submit login"/>
+           </div>
+
 }
 
-export default Test
+export default Login
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import {Link, graphql} from "gatsby"
 import Test from "../components/login_component"
+import Login from "../components/login_component"
 
-export default function Home() {
-  return <div>Hello world!
-    
+export default function Main() {
+  return <div>
+          <Login/>
   </div>
 
 }
